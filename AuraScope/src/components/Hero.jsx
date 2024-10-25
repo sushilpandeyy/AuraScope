09d4e7f4f4208e838e8037a04f7df02fc4a27f51
Govind Mohan Shah <www.govind020@gmail.com>
2024-10-26 03:20:40 +0530

    added front

--- AuraScope/src/components/Hero.jsx
+++ AuraScope/src/components/Hero.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Example from './Illustrator'
+import Study from '../assets/Animation - 1729892186721.json';
 
 const Hero = () => {
   return (
+    <>
+      <div className="hidden md:block md:w-1/2 mt-8 md:mt-0">
+        <Example animationData={Study} />
+      </div>
     
+    </>
   )
 }
 
