8245aa975fd3eea4a6fda450665d184c080c6523
Contactsushil <contact.sushilpandey@gmail.com>
2024-10-25 19:59:32 +0530

    Functions

--- AuraScopeBack/src/routes/home.ts
+++ AuraScopeBack/src/routes/home.ts
@@ -6,4 +6,11 @@ router.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the TypeScript Backend Server!');
 });
 
+router.post('/login', (req: Request, res: Response) => {
+    res.send('Login')
+})
+router.post('/Signup', (req: Request, res: Response) => {
+    res.send('Signup')
+})
+
 export default router;
