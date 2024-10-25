245d5ce2ef5c3895afe61e0f29adfe70d33b9c1a
Contactsushil <contact.sushilpandey@gmail.com>
2024-10-25 23:04:08 +0530

    GEMINI

--- AuraScopeBack/src/routes/home.ts
+++ AuraScopeBack/src/routes/home.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { signup, login } from '../functions/login';
-
+import { modal } from '../functions/model';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
@@ -14,6 +14,10 @@ router.post('/login', (req: Request, res: Response) => {
 // Update the signup route
 router.post('/signup', (req: Request, res: Response) => {
     signup(req, res); // Directly call the signup function
+});
+
+router.post('/modal',(req: Request, res: Response) => {
+    modal(req, res);
 });
 
 export default router;
